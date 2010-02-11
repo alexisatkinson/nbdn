82971087b29322b34e0acb31669840b41eafe4aa
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-02-11 09:23:40 -0700

    Added the CommandRegistrySpecs

--- product/nothinbutdotnetstore.tests/web/CommandRegistrySpecs.cs
+++ product/nothinbutdotnetstore.tests/web/CommandRegistrySpecs.cs
@@ -1,39 +1,32 @@
- using developwithpassion.bdd.contexts;
- using developwithpassion.bdd.harnesses.mbunit;
- using developwithpassion.bdddoc.core;
- using nothinbutdotnetstore.web;
- using nothinbutdotnetstore.web.core;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.harnesses.mbunit;
+using developwithpassion.bdddoc.core;
+using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.tests.web
- {   
-     public class CommandRegistrySpecs
-     {
-         public abstract class concern : observations_for_a_sut_with_a_contract<CommandRegistry,
-                                             DefaultCommandRegistry>
-         {
-        
-         }
+{
+    public class CommandRegistrySpecs
+    {
+        public abstract class concern : observations_for_a_sut_with_a_contract<CommandRegistry,
+                                            DefaultCommandRegistry>
+        {
+        }
 
-         [Concern(typeof(DefaultCommandRegistry))]
-         public class when_getting_a_command_that_can_process_a_request_and_it_has_the_command : concern
-         {
-             context c = () =>
-             {
-            
-             };
+        [Concern(typeof (DefaultCommandRegistry))]
+        public class when_getting_a_command_that_can_process_a_request_and_it_has_the_command : concern
+        {
+            context c = () =>
+            {
+            };
 
-             because b = () =>
-             {
-        
-             };
+            because b = () =>
+            {
+            };
 
-        
-             it should_return_the_command_that_can_do_the_processing = () =>
-             {
-                 
-            
-            
-             };
-         }
-     }
- }
+
+            it should_return_the_command_that_can_do_the_processing = () =>
+            {
+            };
+        }
+    }
+}
